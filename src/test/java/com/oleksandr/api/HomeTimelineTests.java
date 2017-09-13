4fe3d52cf4088d6d774b18b4961c1a02dec65555
afurman <v-afurman@hotwire.com>
2017-09-13 13:59:51 +0300

    Implemented Retweet status test

--- src/test/java/com/oleksandr/api/HomeTimelineTests.java
+++ src/test/java/com/oleksandr/api/HomeTimelineTests.java
@@ -13,11 +13,12 @@ public class HomeTimelineTests {
     private String statusMsg = "Twitter test message1";
     private Timeline timeline;
     private Date createdDate;
+    private Tweets tweets;
 
     @Before
     public void Init() {
         timeline = new Timeline();
-        Tweets tweets = new Tweets();
+        tweets = new Tweets();
         List<Status> status = timeline.GetStatusFromUserTimeline(statusMsg);
         if (status.size() > 0) {
             tweets.DestroyStatus(status.get(0).getId());
@@ -30,5 +31,13 @@ public class HomeTimelineTests {
     public void HomeTimeline_JustCreatedStatus_ShouldHaveCorrectDate() {
         List<Status> status = timeline.GetStatusFromUserTimeline(statusMsg);
         assertEquals(createdDate.getTime(), status.get(0).getCreatedAt().getTime(), 13);
+    }
+
+    @Test
+    public void HomeTimeline_RetweetStatus_ShouldIncreaseRetweetCount() {
+        Status status = timeline.GetStatusFromUserTimeline(statusMsg).get(0);
+        int retweetCount = status.getRetweetCount();
+        tweets.RetweetStatus(status.getId());
+        assertEquals(retweetCount + 1, timeline.GetStatusFromUserTimeline(statusMsg).get(0).getRetweetCount());
     }
 }
